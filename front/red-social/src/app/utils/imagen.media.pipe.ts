01e23fe14da5e9a49e83496b0ad2d3bc5e790327
EzequielSineriz <ezequielsineriz@gmail.com>
2026-06-23 18:39:14 -0300

    actualizacion

--- front/red-social/src/app/utils/imagen.media.pipe.ts
+++ front/red-social/src/app/utils/imagen.media.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { environment } from '../../environments/enviroment'; // Asegurate de importar tu environment
+import { environment } from '../../environments/enviroment';
 
 @Pipe({
   name: 'imagenMedia',
@@ -9,17 +9,14 @@ export class ImagenMediaPipe implements PipeTransform {
 
   transform(urlOriginal: string): string {
     if (!urlOriginal) {
-      return 'assets/images/default-avatar.png'; // Un fallback por si no hay imagen
+      return 'assets/images/default-avatar.png';
     }
 
-    // Si la URL contiene localhost, la interceptamos y le clavamos la URL de Render
     if (urlOriginal.includes('localhost:3000')) {
-      const pathRelativo = urlOriginal.split('localhost:3000')[1]; // Se queda con "/uploads/..."
+      const pathRelativo = urlOriginal.split('localhost:3000')[1];
       return `${environment.apiUrl.replace('/api', '')}${pathRelativo}`;
-      // Si tu apiUrl es 'https://tu-backend.onrender.com/api', esto genera 'https://tu-backend.onrender.com/uploads/...'
     }
 
-    // Si ya viene con el path relativo del Servidor
     if (urlOriginal.startsWith('/uploads')) {
       return `${environment.apiUrl.replace('/api', '')}${urlOriginal}`;
     }
