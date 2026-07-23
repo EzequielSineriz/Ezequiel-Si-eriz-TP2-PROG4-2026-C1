ee7a1bb13c6d20277d32894883f61856b159a284
EzequielSineriz <ezequielsineriz@gmail.com>
2026-07-23 10:58:04 -0300

    cambio enviroments

--- front/red-social/src/app/home/publicaciones/comentarios/comentarios.service.ts
+++ front/red-social/src/app/home/publicaciones/comentarios/comentarios.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IComentario } from '../comentarios/comentarios.interfaces';
-import { environment } from '../../../../environments/enviroment.development';
+import { environment } from '../../../../environments/enviroment';
 
 @Injectable({
   providedIn: 'root'
